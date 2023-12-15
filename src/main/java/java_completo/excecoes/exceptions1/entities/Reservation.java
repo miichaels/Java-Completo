c09a131b288229e682df24294d72b7243f0aa701
miichaels <michael_araujo2012@hotmail.com>
2023-12-15 19:52:28 -0300

    Versao muito ruim da solução

---
+++ src/main/java/java_completo/excecoes/exceptions1/entities/Reservation.java
@@ -0,0 +1,67 @@
+package java_completo.excecoes.exceptions1.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public void Reservation(){
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    // Diferenca entre as duas datas em milisegundos
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        //Converte o diff de miliseconds para dias
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+     @Override
+    public String toString(){
+        return "Room "
+                + roomNumber
+                + ", Check-in: "
+                + sdf.format(checkIn)
+                + ", Check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " Nights";
+     }
+
+
+}
