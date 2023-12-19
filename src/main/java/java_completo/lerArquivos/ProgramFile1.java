d15980bf427548bbb13737e91d8dd706a2ceeb9d
miichaels <michael_araujo2012@hotmail.com>
2023-12-19 17:10:22 -0300

    Lendo arquivo local

---
+++ src/main/java/java_completo/lerArquivos/ProgramFile1.java
@@ -0,0 +1,27 @@
+package java_completo.lerArquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProgramFile1 {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
